48d355b67c2d49566c75e88b32361e2ffddb8ff9
dannuver <dannuver@gmail.com>
2025-07-04 17:29:20 -0500

    funcionsuma

--- contracts/calculadora/src/test.rs
+++ contracts/calculadora/src/test.rs
@@ -1,23 +1,17 @@
 #![cfg(test)]
 
 use super::*;
-use soroban_sdk::{vec, Env, String};
+use soroban_sdk::{Env};
 
 #[test]
-fn test() {
+fn test_suma_y_resultado() {
     let env = Env::default();
     let contract_id = env.register(Contract, ());
     let client = ContractClient::new(&env, &contract_id);
 
-   let resultado = client.sumar(&10, &20);
-    
-     assert_eq!(
-       resultado,
-       30
-    ); 
-    
-    assert_eq!(
-        client.resultado_anterior(),
-        30
-     ); 
+    let resultado = client.sumar(&10, &20);
+    assert_eq!(resultado, 30);
+
+    let anterior = client.resultado_anterior();
+    assert_eq!(anterior, 30);
 }
